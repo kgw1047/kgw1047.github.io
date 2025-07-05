92ce41f125c193e1acd78afd95a28fdcfd27e2cc
kgw1047 <kgw1047@gmail.com>
2025-07-05 18:10:18 +0900

    TEST

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Aligning Protein Conformation Ensemble Generation with Physical Feedback",
+        "excerpt":"ICML 2025. [Paper] Jiarui Lu, Xiaoyin Chen, Stephen Zhewen Lu, Aurélie Lozano, Vijil Chenthamarakshan, Payel Das, Jian Tang 30 May 2025 Keypoints Diffusion-based Generative Model + Energy-based Alignment Boltzmann Factor: 분배함수 계산을 피하기 위해 두 구조의 에너지 차이를 활용 Finite-state Approximation: 전체 분배함수 계산이 불가능하므로 미니배치 단위로 분배함수 계산 Loss...","categories": ["논문리뷰"],
+        "tags": ["Protein Ensemble","Diffusion Model","ICML"],
+        "url": "/%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0/Aligning-Protein-Conformation-Ensemble-Generation-with-Physical-Feedback/",
+        "teaser": null
+      }]
