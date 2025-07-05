01490b92bccd7dd40dec5915898a93d468bd0285
kgw1047 <kgw1047@gmail.com>
2025-07-05 18:26:44 +0900

    TEST

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Aligning Protein Conformation Ensemble Generation with Physical Feedback",
-        "excerpt":"ICML 2025. [Paper] Jiarui Lu, Xiaoyin Chen, Stephen Zhewen Lu, Aurélie Lozano, Vijil Chenthamarakshan, Payel Das, Jian Tang 30 May 2025 Keypoints Diffusion-based Generative Model + Energy-based Alignment Boltzmann Factor: 분배함수 계산을 피하기 위해 두 구조의 에너지 차이를 활용 Finite-state Approximation: 전체 분배함수 계산이 불가능하므로 미니배치 단위로 분배함수 계산 Loss...","categories": ["논문리뷰"],
+        "excerpt":"   ICML 2025. [Paper]  Jiarui Lu, Xiaoyin Chen, Stephen Zhewen Lu, Aurélie Lozano, Vijil Chenthamarakshan, Payel Das, Jian Tang  30 May 2025    Keypoints  Diffusion-based Generative Model + Energy-based Alignment     Boltzmann Factor: 분배함수 계산을 피하기 위해 두 구조의 에너지 차이를 활용   Finite-state Approximation: 전체 분배함수 계산이 불가능하므로 미니배치 단위로 분배함수 계산   Loss Function:   Limitations     AlphaFold3 구조의 모델을 사용했기 때문에 long time generation에 적합하지 않다.   Energy, 즉 forcefield의 정확도 문제.   Monomer만 가능.   다른 generative model의 가능성(flow matching 같은)   References  Protein Conformation Generations     Boltzmann generator   EigenFold   Str2Str   DiG   ConfDiff   AlphaFlow   ESMDiff   MDGen   Alignment methods for generative models     Diffusion-DPO   ABDPO   ALIDIFF   DECOMPDPO  ","categories": ["논문리뷰"],
         "tags": ["Protein Ensemble","Diffusion Model","ICML"],
         "url": "/%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0/Aligning-Protein-Conformation-Ensemble-Generation-with-Physical-Feedback/",
         "teaser": null
